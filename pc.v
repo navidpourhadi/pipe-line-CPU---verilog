89400ce0a9d6f0fbd655421fad95c0d8d53bb8f5
navidpourhadi <40823648+navidpourhadi@users.noreply.github.com>
2018-12-16 10:25:14 +0330

    Add files via upload

--- pc.v
+++ pc.v
@@ -1,14 +1,14 @@
-module pc(input reset,input write,input clk,input [63:0] pcPre,output reg [63:0] pcNext);
-
-	always @(posedge clk)
-		begin
-			if(reset)
-				begin
-					pcNext=0;
-					//reset=0;
-				end
-			else if(write)
-				pcNext=pcPre;
-			
-		end
+module pc (input clk,[63:0] bpc,reg wpc,reset,output reg [63:0] apc);
+  initial begin
+  reset = 1;
+  wpc = 1;
+  end
+  always @(posedge clk) begin
+    if (reset) 
+      begin
+      apc = 0;
+      reset = 0;
+      end
+    else apc = bpc;
+  end
 endmodule
