4743dd6bb14fc2738e1f4b0a694b297461e34da9
navidpourhadi <40823648+navidpourhadi@users.noreply.github.com>
2018-12-16 11:12:49 +0330

    Add files via upload

--- pc.v
+++ pc.v
@@ -1,14 +1,10 @@
 module pc (input clk,[63:0] bpc, wpc,reset,output reg [63:0] apc);
-  initial begin
-  reset = 1;
-  wpc = 1;
-  end
-  always @(posedge clk) begin
-    if (reset) 
-      begin
-        apc = 0;
-      end
-    else if 
-        apc = bpc;
-  end
+	
+  	always @(posedge clk) begin
+    		if (reset) begin
+      			apc = 0;
+      		end
+    		else if(wpc)
+	 		apc = bpc;
+  	end
 endmodule
--- registerBank32x64.v
+++ registerBank32x64.v
@@ -1,11 +1,11 @@
-module regBank32x64(input [4:0] a ,input [4:0] b ,input [4:0] c ,input w ,output  [63:0] dataA ,output  [63:0] dataB ,input [63:0] dataC ,input clk);
+module regBank32x64(input [4:0] a ,input [4:0] b ,input [4:0] c ,input w ,output [63:0] dataA ,output [63:0] dataB ,input [63:0] dataC ,input clk);
 	reg [63:0] regbank [0:31];
+	assign dataA = regbank[a];
+	assign dataB = regbank[b];
 
-	assing	dataA = regbank[a];
-	assing	dataB = regbank[b];
 
 	always @(posedge clk) begin
-
+		
 		if (w==1) regbank[c] = dataC;
 	end
 endmodule
