0a0cde4378d497ae46cc6ee0c62c38c8c1c820ec
navidpourhadi <40823648+navidpourhadi@users.noreply.github.com>
2018-12-23 10:50:11 +0330

    Add files via upload

--- CPU.v
+++ CPU.v
@@ -11,18 +11,19 @@ module CPU();
 	pc pc0(clk,inputPc,writepc,reset,outputPc);
 	adder adder0(outputPc,4,result1InPc);
 	instructionMemory ins_mem(outputPc , instruction);
+	control_unit control0(instruction[31:21],Reg2Loc, ALUSrc , MemtoReg , RegWrite , MemRead , MemWrite , Branch , ALUOp1 , ALUop0);
 	MUX mux0(instruction [20:16] , instruction [9:5] , Reg2Loc , addr2reg);
 	registerBank32x64 regBank0(addr1reg , addr2reg , instruction[4:0] ,RegWrite , dataA , dataB , dataC , clk);
 	signExtend signextend0(instruction , signExOut);
 	MUX mux1(signExOut , dataB , ALUSrc , ALU2In);
-	alu_control(ALUOp0 , ALUOp1 , instruction[31:21] ,ALUControl );
-	alu alu0(dataA , ALU2In , ALUControl , ALUOut , zero);
+	AluControl alucontrol0(ALUOp0 , ALUOp1 , instruction[31:21] ,ALUControl );
+	ALU alu0(dataA , ALU2In , ALUControl , ALUOut , zero);
 	shift2 shift(signExOut , shiftOut);
 	adder adder1(shiftOut , outputPc , result2InPc);
 	MUX mux2(result1InPc , result2InPc , zero & Branch , inputPc);
-	datamemory datamemory0(clk , ALUOut, dataB , MemRead , MemWrite , outputDataMem);
+	DataMemory datamemory0(clk , ALUOut, dataB , MemRead , MemWrite , outputDataMem);
 	MUX mux3(ALUOut , outputDataMem , MemtoReg , dataC);
-	control control0(instruction[31:21],Reg2Loc, ALUSrc , MemtoReg , RegWrite , MemRead , MemWrite , Branch , ALUOp1 , ALUop0);
+
 
 endmodule
 
--- instructionMemory.v
+++ instructionMemory.v
@@ -6,6 +6,10 @@ module instructionMemory (input[63:0] outpc,output[31:0] instruction);
 	initial
 	begin
 		for (i=0; i<32; i=i+1) memory[i] = 32'b0;
+		memory[0]=32'h8B1F03E5;
+		memory[1]=32'hF84000A4;
+		memory[2]=32'h8B040086;
+		memory[3]=32'hF80010A6;
 	end
 	
 	assign instruction =  memory[outpc[6:2]];
